eda013145aeb3955fb87745728305c305cb779e4
Komal Devrapalli <komaldevlapeeli@gmail.com>
2026-04-03 23:48:55 +0530

    added api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://hostel-backendcv-zed6.onrender.com/";
+const BASE_URL = "https://hostel-backendcv-zed6.onrender.com/api";
 
 const handleResponse = async (response) => {
   if (response.status === 401) {
