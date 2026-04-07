782d8bb9615ae99c2a5c6d0d7dea3688c9aa8b1d
Komal Devrapalli <komaldevlapeeli@gmail.com>
2026-04-07 18:30:36 +0530

    fix socket dependency

--- frontend/src/pages/FeeDashboard.jsx
+++ frontend/src/pages/FeeDashboard.jsx
@@ -4,7 +4,7 @@ import MainLayout from "../components/layout/MainLayout";
 import toast from "react-hot-toast";
 import { API } from "../services/apiRoutes";
 import { io } from "socket.io-client";
-
+console.log("🔥 SOCKET CODE LOADED");
 const socket = io("https://backend-qlmf.onrender.com", {
   transports: ["websocket"],   // 🔥 force websocket
   secure: true,
