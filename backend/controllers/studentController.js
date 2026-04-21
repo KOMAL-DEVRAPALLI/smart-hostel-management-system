11ff39aa232b96d822420eec17cb0e2875047a75
KOMAL-DEVRAPALLI <155255513+KOMAL-DEVRAPALLI@users.noreply.github.com>
2026-04-21 10:10:45 +0530

    Log error in student addition process

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -48,7 +48,7 @@ export const addStudent = async (req, res) => {
     });
 
   } catch (error) {
-
+   console.log(error)
     res.status(500).json({
       message: "Error adding student",
     });
